7453ebbbc5edeb4972a5c342824f0d43070e58e0
Glauber Farias <desglauber@gmail.com>
2024-04-04 15:48:06 -0400

    Conhecendo Collections Java - Ordenação Liste: Testando os métodos

--- src/list/Ordenacao/OrdenacaoPessoas.java
+++ src/list/Ordenacao/OrdenacaoPessoas.java
@@ -5,6 +5,20 @@ import java.util.Collections;
 import java.util.List;
 
 public class OrdenacaoPessoas {
+
+    public static void main(String[] args) {
+        OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+        ordenacaoPessoas.adicionarPessoa("Nome 1", 20, 1.56);
+        ordenacaoPessoas.adicionarPessoa("Nome 2", 30, 1.80);
+        ordenacaoPessoas.adicionarPessoa("Nome 3", 25, 1.70);
+        ordenacaoPessoas.adicionarPessoa("Nome 4", 17, 1.56);
+
+        System.out.println(ordenacaoPessoas.pessoaList);
+
+        System.out.println(ordenacaoPessoas.ordenarPorIdade());
+        System.out.println(ordenacaoPessoas.ordenarPorAltura());
+    }
+
     //atributos
 
     private List<Pessoa> pessoaList;
