ea50f7cddb6b62fac214e64c92ff144e38b848a7
Glauber Farias <desglauber@gmail.com>
2024-04-04 16:14:57 -0400

    Operações Básicas Set

---
+++ src/set/OperacoesBasicas/Convidado.java
@@ -0,0 +1,22 @@
+package set.OperacoesBasicas;
+
+public class Convidado {
+    //atributos
+
+    private String nome;
+    private int codigoConvite;
+    public Convidado(String nome, int codigoConvite) {
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public int getCodigoConvite() {
+        return codigoConvite;
+    }
+    @Override
+    public String toString() {
+        return "Convidados [nome=" + nome + ", codigoConvite=" + codigoConvite + "]";
+    }
+}
