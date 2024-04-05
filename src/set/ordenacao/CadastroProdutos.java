77cf5071032d57ad72ae224b45701c5e1f9f5977
Glauber Farias <desglauber@gmail.com>
2024-04-05 16:28:39 -0400

    Ordenação Set: testando os métodos

--- src/set/ordenacao/CadastroProdutos.java
+++ src/set/ordenacao/CadastroProdutos.java
@@ -7,7 +7,17 @@ import java.util.TreeSet;
 public class CadastroProdutos {
 
     public static void main(String[] args) {
-        
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+
+        cadastroProdutos.adicionarProduto(1L, "Produto 5", 15d, 5);
+        cadastroProdutos.adicionarProduto(2L, "Produto 0", 20d, 10);
+        cadastroProdutos.adicionarProduto(1L, "Produto 3", 10d, 2);
+        cadastroProdutos.adicionarProduto(9L, "Produto 9", 2d, 2);
+
+        System.out.println(cadastroProdutos.produtosSet);
+
+        System.out.println("Produtos por Nome: " + cadastroProdutos.exibirProdutosPorNome());
+        System.out.println("Produtos por Preço: " + cadastroProdutos.exibirProdutosPorPreco());
     }
 
     //atributos
@@ -29,7 +39,7 @@ public class CadastroProdutos {
 
     public Set<Produto> exibirProdutosPorPreco() {
         Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorProPreco());
-        produtosSet.addAll(produtosSet);
+        produtosPorPreco.addAll(produtosSet);
         return produtosPorPreco;
     }
 }
