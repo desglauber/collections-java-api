743bcf486ea75d19b4aeb98926787514e016eb76
Glauber Farias <desglauber@gmail.com>
2024-04-08 15:11:02 -0400

    Operações Básicas Map: testando os métodos

--- src/map/OperacoesBasicas/AgendaContatos.java
+++ src/map/OperacoesBasicas/AgendaContatos.java
@@ -4,6 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AgendaContatos {
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos(null);
+
+        agendaContatos.adicionarContato("Glauber", 91234567);
+        agendaContatos.adicionarContato("Yara", 91237890);
+        agendaContatos.adicionarContato("Celeste", 91239876);
+        agendaContatos.adicionarContato("Douglas", 91236543);
+        agendaContatos.adicionarContato("Glauber", 91236547);
+        agendaContatos.adicionarContato("Glauber", 91235642);
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.removerContato("Celeste");
+
+        agendaContatos.exibirContatos();
+
+        System.out.println("O número é: " + agendaContatos.pesquisarPorNome("Yara"));
+    }
+
     //atributos
 
     private Map<String, Integer> agendaContatoMap;
@@ -18,6 +38,7 @@ public class AgendaContatos {
 
     public void removerContato(String nome) {
         if (!agendaContatoMap.isEmpty()) {
+            System.out.println("O contato de " + nome + " foi removido com sucesso!");
             agendaContatoMap.remove(nome);    
         }
     }
@@ -30,6 +51,7 @@ public class AgendaContatos {
         Integer numeroPorNome = null;
         if (!agendaContatoMap.isEmpty()) {
             numeroPorNome = agendaContatoMap.get(nome);
+            System.out.println("O nome pesquisado foi: " + nome);
         }
         return numeroPorNome;
     }
